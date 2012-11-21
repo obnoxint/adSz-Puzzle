f0124787b2af6960eda0c8cea10d62dca46a8260
obnoxint <mail@obnoxint.net>
2012-11-21 10:26:12 +0100

    Raised version number.

--- src/net/obnoxint/adsz/puzzle/Main.java
+++ src/net/obnoxint/adsz/puzzle/Main.java
@@ -34,7 +34,7 @@ import org.newdawn.slick.UnicodeFont;
 
 public final class Main {
 
-    public static final int VERSION = 1;
+    public static final int VERSION = 2;
 
     static final int EXIT_CODE_ERROR = -1;
     static final int EXIT_CODE_OK = 0;
